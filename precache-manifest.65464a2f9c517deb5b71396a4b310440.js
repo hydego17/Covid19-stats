ef867c669d46770cb25ff2f3039221673b4123a0
Umma Ahimsha <uahimsha@gmail.com>
2020-09-23 12:57:52 +0700

    Updates

---
+++ precache-manifest.65464a2f9c517deb5b71396a4b310440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3aeb990668759dce398f94388a29d48",
+    "url": "/covid19-stats/index.html"
+  },
+  {
+    "revision": "f30722450a807d413abb",
+    "url": "/covid19-stats/static/css/main.2208100e.chunk.css"
+  },
+  {
+    "revision": "9ed9f09155221a9683b8",
+    "url": "/covid19-stats/static/js/2.790aa534.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid19-stats/static/js/2.790aa534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30722450a807d413abb",
+    "url": "/covid19-stats/static/js/main.3b671e42.chunk.js"
+  },
+  {
+    "revision": "f867e7baedf054211a24",
+    "url": "/covid19-stats/static/js/runtime-main.092348a2.js"
+  },
+  {
+    "revision": "2576fc595ff2034918a3a388a83ba0df",
+    "url": "/covid19-stats/static/media/image1.2576fc59.jpg"
+  },
+  {
+    "revision": "469b8a441e5b64f8206d84959e521f7c",
+    "url": "/covid19-stats/static/media/virus.469b8a44.svg"
+  }
+]);
